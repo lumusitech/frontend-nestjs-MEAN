12fc938936249c13dc8e089de060e38d568e265b
lumusitech <lumusitech@gmail.com>
2023-11-01 21:57:49 -0300

    feat: add base project

---
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,21 @@
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardLayoutComponent,
+    // children: [],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class DashboardRoutingModule {}
