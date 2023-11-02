57b55cc4fcf5b656479ea4f49c08334570e7e665
lumusitech <lumusitech@gmail.com>
2023-11-01 22:36:34 -0300

    feat: add login design and formBuilder

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent {
+  private fb = inject(FormBuilder);
 
+  public myForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  login() {
+    console.log(this.myForm.value);
+  }
 }
